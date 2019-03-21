37bc81e1cdcf1910ddd01f22052760cbfd21deb5
ddpbsd <ddpbsd@gmail.com>
2019-03-21 10:34:49 -0400

    I prefer if statements with braces.

--- src/os_net/os_net.c
+++ src/os_net/os_net.c
@@ -841,8 +841,9 @@ int setnonblock(int fd) {
     int flags;
 
     flags = fcntl(fd, F_GETFL);
-    if (flags < 0)
+    if (flags < 0) {
         return flags;
+    }
     flags |= O_NONBLOCK;
     if (fcntl(fd, F_SETFL, flags) < 0) {
         return -1;
